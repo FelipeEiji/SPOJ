491cebc53e2b399138fcb4bfdb1eafe68238318e
Felipe Eiji Maruyama <37049847+FelipeEiji@users.noreply.github.com>
2020-03-05 14:40:44 +0100

    Create EIGHTS.cpp
    Solution for the problem EIGHTS of SPOJ in C++

---
+++ EIGHTS.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int lli;
+
+int main(int argc, char** argv) {
+	
+	int t;
+	lli k;
+	
+	cin >> t;
+	
+	while(t--)
+	{
+		cin >> k;
+		cout << 192+(k-1)*250 << endl;
+	}
+	
+	return 0;
+}
